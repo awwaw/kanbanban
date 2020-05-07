1c31974e94d8ccbdee6d7b7a25e04700aff9994c
ddespairrr <vovapodkorytov966@gmail.com>
2020-05-07 15:25:50 +0500

    аааа

---
+++ static/scripts/sc1.js
@@ -0,0 +1,15 @@
+let dropdownFunction = () => {
+	document.getElementById("myDropdown").classList.toggle("show");
+};
+
+window.onclick = (event) => {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementByClassName("dropdown-content");
+		for (let i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
