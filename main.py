9b60ba9847ff41a1da334044e5826af1a6e6a704
ddespairrr <vovapodkorytov966@gmail.com>
2020-05-13 00:32:23 +0500

    ОНО РАБОТАЕТ POGGERS

--- main.py
+++ main.py
@@ -248,4 +248,6 @@ def join(id):
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
-    app.run(port=8080, host='127.0.0.1')
+    # app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
